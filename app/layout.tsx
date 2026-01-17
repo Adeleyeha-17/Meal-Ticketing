031258608c95b437999a03e7ab4a71a553ccd8b1
EDEN <adeleyeha.17@protonmail.com>
2026-01-17 18:14:20 +0100

    added icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Meal Ticketing",
   description: "ELIZADE NIGERIA LIMITED MEAL TICKETING SYSTEM",
+    icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  }
 };
 
 export default function RootLayout({
